479deaa612f5075df8004425e2c3af976c3b629b
robertocarneiro <robertocarneirosf@gmail.com>
2015-11-23 15:30:37 -0300

    Ajeitando excluir promocoes e status compra

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'iwheyapp@gmail.com',
-    :password             => 'iwheycgd',
+    :user_name            => ENV['IWHEYWEB_EMAIL'],
+    :password             => ENV['IWHEYWEB_EMAIL_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true 
   }
